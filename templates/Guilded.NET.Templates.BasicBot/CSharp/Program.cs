be4c44b853f279b75dbec2a047a7a330ce4b0abc
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2021-08-06 12:14:23 +0300

    Forgot to update templates

---
+++ templates/Guilded.NET.Templates.BasicBot/CSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using Guilded.NET;
+
+using Newtonsoft.Json.Linq;
+
+namespace ProjectName
+{
+    public class Program
+    {
+        /// <summary>
+        /// Creates a new user bot client.
+        /// </summary>
+        /// <param name="args">Program arguments</param>
+        static void Main()
+        {
+            // Gets all of the configuration from the file
+            JObject config = JObject.Parse(File.ReadAllText("./config/config.json"));
+            string token = config["token"].Value<string>(),
+                   prefix = config["prefix"].Value<string>();
+            // Creates new client
+            using GuildedBotClient client = new(token);
+            // When the client connects to Guilded, output "Connected"
+            client.Connected += (o, e) => Console.WriteLine("Connected");
+            // When the client is ready, output "I successfully logged in!"
+            client.Prepared += (o, e) => Console.WriteLine($"I successfully logged in!");
+            // Start the bot
+            RunAsync(client).GetAwaiter().GetResult();
+        }
+        /// <summary>
+        /// Keeps the connection of the Guilded client.
+        /// </summary>
+        /// <param name="client">Client to keep running</param>
+        static async Task RunAsync(GuildedBotClient client)
+        {
+            // Connects to Guilded
+            await client.ConnectAsync();
+            // Makes it stop forever, so the bot wouldn't instantly shutdown after connecting
+            await Task.Delay(-1);
+        }
+    }
+}
