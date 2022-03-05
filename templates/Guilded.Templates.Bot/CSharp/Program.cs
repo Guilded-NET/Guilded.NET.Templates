89cf8e3c63e6157de1d498aa14a12558beba8436
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2022-03-05 20:38:04 +0200

    Update templates to Guilded.NET 0.6.0, .NET 6; rename template

---
+++ templates/Guilded.Templates.Bot/CSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reactive.Linq;
+using Guilded;
+using Newtonsoft.Json.Linq;
+
+// Get the configuration values
+JObject config = JObject.Parse(await File.ReadAllTextAsync("./config/config.json"));
+
+string auth = config.Value<string>("auth")!,
+       prefix = config.Value<string>("prefix")!;
+
+using GuildedBotClient client = new(auth);
+
+client.Prepared += (_, _) => Console.WriteLine("The bot is prepared!");
+
+// Wait for !ping messages
+client.MessageCreated
+    .Where(msgCreated => msgCreated.Content == prefix + "ping")
+    .Subscribe(async msgCreated =>
+        await msgCreated.ReplyAsync("Pong!")
+    );
+
+await client.ConnectAsync();
+
+// Don't close the program when the bot connects
+await Task.Delay(-1);
