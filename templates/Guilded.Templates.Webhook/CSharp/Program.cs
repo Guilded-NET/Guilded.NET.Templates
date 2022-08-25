2857f0c2281ca7dcff23b3298cebe64b896bdee2
IdkGoodName <31628630+IdkGoodName@users.noreply.github.com>
2022-08-25 11:51:08 +0300

    feat: webhook client template

---
+++ templates/Guilded.Templates.Webhook/CSharp/Program.cs
@@ -0,0 +1,5 @@
+﻿using Guilded.Webhook;
+
+await using var webhookClient = new GuildedWebhookClient("...url here...");
+
+await webhookClient.CreateMessageAsync("Guilded.NET webhook client works!");
